a6a2fcbb46795ef4c7c1efaaa50defbe1cfede88
Younggi Kim <roadlamp@gmail.com>
2016-09-23 23:51:12 +0900

    Add Chap3 Prob4 solve

--- discovery_go/chap3/prob4/prob4_test.go
+++ discovery_go/chap3/prob4/prob4_test.go
@@ -1,1 +1,30 @@
 package prob4
+
+import "fmt"
+
+func Example_queue() {
+	q := []string{"c", "d", "e"}
+
+	printSliceInfo(q)
+
+	// push queue
+	q = append(q, "a")
+
+	printSliceInfo(q)
+	// pop queue
+	e := q[0]
+	q = q[1:]
+
+	printSliceInfo(q)
+
+	fmt.Println(e)
+
+	// Output:
+}
+
+func printSliceInfo(q []string) {
+	fmt.Println("======")
+	fmt.Println(q)
+	fmt.Println(len(q))
+	fmt.Println(cap(q))
+}
