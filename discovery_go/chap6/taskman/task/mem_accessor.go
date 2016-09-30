fa1787455365eb711a7fbd0e924fe39e36e6931d
Younggi Kim <roadlamp@gmail.com>
2016-09-30 15:19:52 +0900

    Add Chap6 prob3 solved

--- discovery_go/chap6/taskman/task/mem_accessor.go
+++ discovery_go/chap6/taskman/task/mem_accessor.go
@@ -3,12 +3,14 @@ package task
 import (
 	"errors"
 	"fmt"
+	"sync"
 )
 
 // InMemoryAccessor is a simple in-memory database.
 type InMemoryAccessor struct {
 	tasks  map[ID]Task
 	nextID int64
+	L      *sync.RWMutex
 }
 
 // NewInMemoryAccessor returns a new InMemoryAccessor.
@@ -16,6 +18,7 @@ func NewInMemoryAccessor() Accessor {
 	return &InMemoryAccessor{
 		tasks:  map[ID]Task{},
 		nextID: int64(1),
+		L:      &sync.RWMutex{},
 	}
 }
 
@@ -24,7 +27,9 @@ var ErrTaskNotExist = errors.New("task does not exist")
 
 // Get returns a task with a given ID.
 func (m *InMemoryAccessor) Get(id ID) (Task, error) {
+	m.L.RLock()
 	t, exists := m.tasks[id]
+	m.L.RUnlock()
 	if !exists {
 		return Task{}, ErrTaskNotExist
 	}
@@ -33,6 +38,8 @@ func (m *InMemoryAccessor) Get(id ID) (Task, error) {
 
 // Put updates a task with a given ID with t
 func (m *InMemoryAccessor) Put(id ID, t Task) error {
+	m.L.Lock()
+	defer m.L.Unlock()
 	if _, exists := m.tasks[id]; !exists {
 		return ErrTaskNotExist
 	}
@@ -42,6 +49,8 @@ func (m *InMemoryAccessor) Put(id ID, t Task) error {
 
 // Post adds a new
 func (m *InMemoryAccessor) Post(t Task) (ID, error) {
+	m.L.Lock()
+	defer m.L.Unlock()
 	id := ID(fmt.Sprint(m.nextID))
 	m.nextID++
 	m.tasks[id] = t
@@ -50,6 +59,8 @@ func (m *InMemoryAccessor) Post(t Task) (ID, error) {
 
 // Delete removes the task with a given ID.
 func (m *InMemoryAccessor) Delete(id ID) error {
+	m.L.Lock()
+	defer m.L.Unlock()
 	if _, exists := m.tasks[id]; !exists {
 		return ErrTaskNotExist
 	}
@@ -59,10 +70,11 @@ func (m *InMemoryAccessor) Delete(id ID) error {
 
 // GetAll gets all of the tasks
 func (m *InMemoryAccessor) GetAll() ([]Task, error) {
-	v := make([]Task, len(m.tasks))
-
+	m.L.RLock()
+	v := []Task{}
 	for _, value := range m.tasks {
 		v = append(v, value)
 	}
+	m.L.RUnlock()
 	return v, nil
 }
